ae7e577ecf78b888dc8938dc0760374604bd1769
evorhard <evorhard@gmail.com>
2024-01-15 15:40:28 +0100

    Switched to WSL, no real commits but just want to see it work

--- app/spider.py
+++ app/spider.py
@@ -1,9 +1,21 @@
 import requests
 
-
-def make_request():
-    pass
+from loguru import logger
 
 
-def spider_discogs():
-    pass
+def make_request(url: str) -> requests.Response:
+    response = requests.request("GET", url, timeout=300)
+
+    return response
+
+
+def spider_discogs(album_name: str):
+    album_name = album_name.replace(" ", "+")
+    url = f"https://www.discogs.com/search?q={album_name}&type=all"
+
+    response = make_request(url)
+
+    logger.info(response.text)
+
+
+spider_discogs("saw you for the first time")
