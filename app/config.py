7e7827da0016f842592aa55b8b3d7e4c3be50963
Evert de Ruiter <58728221+evorhard@users.noreply.github.com>
2024-01-23 23:42:02 +0100

    Implemented Zenrows to spider RYM

--- app/config.py
+++ app/config.py
@@ -12,6 +12,7 @@ REDIRECT_URI = os.environ.get("REDIRECT_URI")
 AUTHORIZATION_URL = os.environ.get("AUTHORIZATION_URL")
 TOKEN_URL = os.environ.get("TOKEN_URL")
 API_BASE_URL = os.environ.get("API_BASE_URL")
+ZEN_ROWS = os.environ.get("ZEN_ROWS")
 
 # Other globals
 SCOPE = "user-read-private user-read-email"
--- app/spider.py
+++ app/spider.py
@@ -1,21 +1,35 @@
-import requests
-
 from loguru import logger
+from typing import Dict
+from zenrows import ZenRowsClient
+
+# Local imports
+from config import ZEN_ROWS
 
 
-def make_request(url: str) -> requests.Response:
-    response = requests.request("GET", url, timeout=300)
+def make_request(
+    url: str, timeout: int = 60, headers: Dict = None, data: Dict = None
+) -> str:
+    client = ZenRowsClient(ZEN_ROWS)
+    params = {"js_render": "true", "premium_proxy": "true"}
 
-    return response
+    response = client.get(url, timeout=timeout, params=params)
+
+    logger.info(response.status_code)
+
+    return response.text
 
 
-def spider_discogs(album_name: str):
+def spider_rym(album_name: str):
+    # https://rateyourmusic.com/search?searchterm=the%20soft%20moon&searchtype=
+    # https://rateyourmusic.com/search?searchterm=the+soft+moon&searchtype=l
+
     album_name = album_name.replace(" ", "+")
-    url = f"https://www.discogs.com/search?q={album_name}&type=all"
+    url = f"https://rateyourmusic.com/search?searchterm={album_name}&searchtype=l"
 
     response = make_request(url)
 
-    logger.info(response.text)
+    with open("test.html", "w", encoding="utf-8") as f:
+        f.write(response)
 
 
-spider_discogs("saw you for the first time")
+spider_rym("the soft moon")
